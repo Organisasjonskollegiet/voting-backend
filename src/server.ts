830781f845497bc52f3315d3083bd09057003b34
Endre Braut Medhus <endrebm@stud.ntnu.no>
2021-03-23 21:12:27 +0100

    Teesting

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,26 @@ import { ApolloServer } from 'apollo-server';
 import { stitchSchemas } from '@graphql-tools/stitch';
 import { authSchema } from './schema';
 import simple_mock from './mocks/mock';
+import jwt from 'express-jwt';
+import jwtAuthz from 'express-jwt-authz';
+import jwksRsa from 'jwks-rsa';
+
+const checkJwt = jwt({
+    // Dynamically provide a signing key
+    // based on the kid in the header and
+    // the signing keys provided by the JWKS endpoint.
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-4bxg75mo.eu.auth0.com/.well-known/jwks.json',
+    }),
+
+    // Validate the audience and the issuer.
+    audience: 'https://stemmesystem/api',
+    issuer: 'https://dev-4bxg75mo.eu.auth0.com/',
+    algorithms: ['RS256'],
+});
 
 const isMocking = process.env.MOCKING == 'true';
 
