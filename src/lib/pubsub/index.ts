c35889f4375ad8634061e13df93ac490348042e0
kharann <anhkhav@gmail.com>
2021-09-23 11:35:03 +0200

    add tls option for redis

--- src/lib/pubsub/index.ts
+++ src/lib/pubsub/index.ts
@@ -6,6 +6,9 @@ const options: Redis.RedisOptions = {
     host: process.env.REDIS_HOST,
     password: process.env.REDIS_PASSWORD,
     port: 6380,
+    tls: {
+        servername: process.env.REDIS_HOST,
+    },
     retryStrategy: (times: number) => {
         // reconnect after
         return Math.min(times * 50, 2000);
