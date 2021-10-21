5ed48408429ea877ccae6acc9f56cf384b599bb4
kharann <anhkhav@gmail.com>
2021-10-21 17:11:01 +0200

    feat: remove cors protection due to CORS protection from azure

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import simpleMock from './lib/mocks/mock';
 import { protectedSchema } from './schema';
-import cors from 'cors';
 import 'dotenv/config';
 import { Context } from './context';
 import { checkJwt, DecodedToken } from './lib/auth/verifyToken';
@@ -35,32 +34,13 @@ const allowedOrigins = ['https://ecclesia.netlify.app', 'https://vaas.azurewebsi
 
 export const createGraphqlServer = async (server: ApolloServer, prisma: PrismaClient) => {
     const app = express();
-    app.use(
-        cors({
-            origin: (origin, callback) => {
-                if (process.env.NODE_ENV == 'development') {
-                    allowedOrigins.push('http://localhost:4000', 'http://localhost:3000');
-                }
-                // allow requests with no origin
-                // (like mobile apps or curl requests)
-                if (!origin) return callback(null, true);
-                // Either in the list of allowed origins, a preview deployment from netlify or on localhost
-                if (allowedOrigins.includes(origin) || origin.match(/https:\/\/([\w-]+)--ecclesia.netlify.app/g)) {
-                    callback(null, true);
-                } else {
-                    const msg = 'The CORS policy for this site does not ' + 'allow access from the specified Origin.';
-                    return callback(new Error(msg), false);
-                }
-            },
-        })
-    );
     app.use(checkJwt);
 
     if (process.env.MOCKING != 'true') await prisma.$connect();
     // Connect to database
     if (process.env.NODE_ENV != 'development') await prisma.$connect();
     // We need to turn the express app into an httpserver to use websockets.
-    // We also overwrite  Apollo Server's inbult cors
+    // We also overwrite  Apollo Server's inbult cors, we set CORS on Azure
     server.applyMiddleware({ app, path: '/graphql', cors: false });
     return app;
 };
