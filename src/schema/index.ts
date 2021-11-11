56a34d63b5e2c5928f96d5274f697fa4b488438e
kharann <anhkhav@gmail.com>
2021-11-11 10:58:23 +0100

    delete unused import

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,14 +3,13 @@ import { join } from 'path';
 import * as UserSchema from './auth/';
 import * as VotationSchema from './votation/';
 import * as MeetingSchema from './meeting';
-import * as ViewSchema from './views';
 import * as Enums from './enums';
 import { GQLDateTime } from './scalars';
 import { applyMiddleware } from 'graphql-middleware';
 import permissions from '../lib/permissions';
 
 const baseSchema = makeSchema({
-    types: [GQLDateTime, Enums, UserSchema, VotationSchema, MeetingSchema, ViewSchema], // 1
+    types: [GQLDateTime, Enums, UserSchema, VotationSchema, MeetingSchema], // 1
     outputs: {
         typegen: join(__dirname, '../__generated__', 'nexus-typegen.ts'), // 2
         schema: join(__dirname, '../__generated__', 'schema.graphql'), // 3
